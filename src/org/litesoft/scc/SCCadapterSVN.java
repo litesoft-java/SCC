46f58d3aba702a5c91fd1f6a9cb43b62c3ac64b9
Mark Chandler <markc6275@gmail.com>
2014-11-02 17:04:11 -0800

    Closer to working.

--- src/org/litesoft/scc/SCCadapterSVN.java
+++ src/org/litesoft/scc/SCCadapterSVN.java
@@ -22,11 +22,11 @@ public class SCCadapterSVN extends AbstractSCCadapter {
     @Override
     public CommandResults status( DirectoryResults pResults ) {
         reportProgress();
-        List<String> zLines = runCommand( pResults, "-u status" );
+        List<String> zLines = runCommand( pResults, "-u status" ); // -u always gets some output if everything is working correctly.
         if ( zLines == null ) {
             return Error;
         }
-//    *      975   Java/ScarPlus/src/com/esotericsoftware/scar/Scar.java
+//    *              975   Java/ScarPlus/src/com/esotericsoftware/scar/Scar.java
 //    M              975   Java/KeyHole/KeyHole.ipr
 //    M              975   Java/DATT/DATT.ipr
 //    ?                    Java/DATT/src/DiagramTest
@@ -41,10 +41,26 @@ public class SCCadapterSVN extends AbstractSCCadapter {
     @Override
     public CommandResults update( DirectoryResults pResults ) {
         reportProgress();
-        List<String> zLines = runCommand( pResults, "update" );
+        List<String> zLines = runCommand( pResults, "update --accept postpone" );
         if ( zLines == null ) {
             return Error;
         }
+// svn update --accept p
+//        Updating '.':
+//        C    ztest.txt
+//        Updated to revision 980.
+//        Summary of conflicts:
+//        Text conflicts: 1
+        // Creates 3 more files (*.mine, *.r{previous_version}, *.r{current_repo_version}) and the original file with merge conflict markers
+        // Therefore: If we see the word "conflicts" then we need to dump the whole output to the error space.
+        //            Errors will need to be fixed before running again. (Can use IntelliJ to fix the errors [merge the three files]). Then run again.
+        // Otherwise: If we see "Updated to revision" then show the whole output to the update space.
+        // Otherwise 2: If we anything other then
+        // "Updating '.':
+        //  At revision XXX."
+        // then it is an error and the output should be added to the error space.
+        // Otherwise 3: Report nothing! Yeah!
+
 //    Updating '.':
 //    At revision 32.
 //
